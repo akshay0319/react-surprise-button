3f069871619082b24ebf1b053ba8ddea4514fbd9
akshay.ekunde@armworldwide.com <akshay.ekunde@armworldwide.com>
2025-03-02 17:02:11 +0530

    Initial commit update

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import SurpriseButton from "./SurpriseButton";
 const App: React.FC = () => {
   return (
     <div>
-      <h1 style={{ textAlign: "center" }}>Get your Surprice by click the button!</h1>
+      <h1 style={{ textAlign: "center" }}>Get your Surprise by click the button!</h1>
       <SurpriseButton />
     </div>
   );
